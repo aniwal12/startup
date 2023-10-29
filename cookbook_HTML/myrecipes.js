52a5c93e1d1e0615dfa61182d64f1bdf623cc0d9
Anika Wallace <anikawallace0828@gmail.com>
2023-10-28 20:09:37 -0600

    made recipe accordion dynamic based off scores.js

---
+++ cookbook_HTML/myrecipes.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function loadRecipes() {
+        let recipes = [];
+        const recipesText = localStorage.getItem('recipes');
+
+        if (recipesText) {
+            recipes = JSON.parse(recipesText);
+        }
+
+        const recipesAccordion = document.querySelector('#Recipes');
+
+        if (recipes.length) {
+            for (const [i, recipe] of recipes.entries()) {
+                const accordionItem = document.createElement('div');
+                accordionItem.className = 'accordion-item';
+
+                const accordionHeader = document.createElement('h2');
+                accordionHeader.className = 'accordion-header';
+                accordionHeader.id = `recipe${i + 1}`;
+
+                const accordionButton = document.createElement('button');
+                accordionButton.className = 'accordion-button collapsed';
+                accordionButton.setAttribute('type', 'button');
+                accordionButton.setAttribute('data-bs-toggle', 'collapse');
+                accordionButton.setAttribute('data-bs-target', `#collapse${i + 1}`);
+                accordionButton.setAttribute('aria-expanded', 'false');
+                accordionButton.setAttribute('aria-controls', `collapse${i + 1}`);
+                accordionButton.textContent = recipe.name;
+
+                accordionHeader.appendChild(accordionButton);
+
+                const accordionBody = document.createElement('div');
+                accordionBody.className = 'accordion-collapse collapse' ;
+                accordionBody.id = `collapse${i+1}`;
+                accordionBody.setAttribute('aria-labelledby', `recipe${i + 1}`);
+                accordionBody.setAttribute('data-bs-parent', '#Recipes');
+
+                const bodyContent = document.createElement('div');
+                bodyContent.className = 'accordion-body';
+                bodyContent.innerHTML = `
+                <p>${recipe.instructions}</p>
+                `;
+
+                accordionBody.appendChild(bodyContent);
+
+                accordionItem.appendChild(accordionHeader);
+                accordionItem.appendChild(accordionBody);
+
+                recipesAccordion.appendChild(accordionItem);
+            }
+        } else {
+            recipesAccordion.innterHTML = '<p>No recipes available</p>'
+        }
+    }
+
+    loadRecipes();
+});
