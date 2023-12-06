4e6634300f025b64cbf9983c838cd9ed184be554
Anika Wallace <anikawallace0828@gmail.com>
2023-12-06 13:55:37 -0700

    updated database.js with login functions

--- database.js
+++ database.js
@@ -1,10 +1,35 @@
 const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 const config = require('./dbConfig.json');
+
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('startup');
+const userCollection = db.collection('user');
 const recipeCollection = db.collection('recipe');
 const requestCollection = db.collection('request');
+
+function getUser(email) {
+    return userCollection.findOne({email: email});
+}
+
+function getUserByToken(token) {
+    return userCollection.findOne({token: token});
+}
+
+async function createUser(email, password) {
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = {
+        email: email,
+        password: passwordHash,
+        token: uuid.v4(),
+    };
+    await userCollection.insertOne(user);
+
+    return user;
+}
 
 async function loadRecipes(currentUser) {
     const query = { username : currentUser}
@@ -32,4 +57,4 @@ async function addRequest(request) {
     return result;
 }
 
-module.exports = { loadRecipes, loadRequests, addRecipe, addRequest };
+module.exports = { getUser, getUserByToken, createUser, loadRecipes, loadRequests, addRecipe, addRequest };
